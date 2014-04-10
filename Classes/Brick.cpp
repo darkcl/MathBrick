7a2cd709dbb493a135b4ea1ec0d5f2f52f5f5952
Yeung Yiu Hung <hkclex@gmail.com>
2014-04-10 22:10:11 +0800

    Initial Commit
    Initial Commit

---
+++ Classes/Brick.cpp
@@ -0,0 +1,9 @@
+//
+//  Brick.cpp
+//  MathBrick
+//
+//  Created by Yeung Yiu Hung on 10/4/14.
+//
+//
+
+#include "Brick.h"
---
+++ Classes/Brick.h
@@ -0,0 +1,14 @@
+//
+//  Brick.h
+//  MathBrick
+//
+//  Created by Yeung Yiu Hung on 10/4/14.
+//
+//
+
+#ifndef __MathBrick__Brick__
+#define __MathBrick__Brick__
+
+#include <iostream>
+
+#endif /* defined(__MathBrick__Brick__) */
